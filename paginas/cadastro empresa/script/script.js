50f25202526e16e98d4ab1b092aee2bb4d6ffea3
Junior <magnojunior319@gmail.com>
2022-05-19 16:10:06 -0400

    finalizado

--- paginas/cadastro empresa/script/script.js
+++ paginas/cadastro empresa/script/script.js
@@ -1,29 +1,4 @@
-const name = document.getElementById('nome')
-const fund = document.getElementById('nasc')
-const cnpj = document.getElementById('cnpj')
-const cnae = document.getElementById('cnae')
-const email = document.getElementById('email')
-const endereco = document.getElementById('endereco')
-const setor_jur = document.getElementById('setor-jur')
-const cep = document.getElementById('cep')
-const comp_end = document.getElementById('comp-endereco')
-const comp_renda = document.getElementById('comp-renda')
-const save = document.getElementById('save')
-const cancel = document.getElementById('cancel')
-const form = [name, fund,cnpj, cnae, email, endereco, setor_jur, cep, comp_end, comp_renda]
-
-
-save.addEventListener('click', () => {
-    for (let i = 0; i < form.length; i++) {
-        if (form[i].value == ""){
-            form[i].style.borderColor = 'red'
-            form[i].setCustomValidity('Campo obrigatório')
-        } else {
-            form[i].style.borderColor = 'green'
-            form[i].required = 'false'
-        }
-    }
-})
+const cancel = document.getElementById('cancel');
 
 cancel.addEventListener('click', () => {
     window.close()
--- paginas/cadastro pessoa fisica/script/script.js
+++ paginas/cadastro pessoa fisica/script/script.js
@@ -1,30 +1,4 @@
-const name = document.getElementById('nome')
-const nascDt = document.getElementById('nasc')
-const cpf = document.getElementById('cpf')
-const email = document.getElementById('email')
-const logradouro = document.getElementById('logradouro')
-const bairro = document.getElementById('bairro')
-const number = document.getElementById('numero')
-const cep = document.getElementById('cep')
-const comp_end = document.getElementById('comp-endereco')
-const cert_crim = document.getElementById('cert-crim')
-const est_civ = document.getElementsByName('est-civ')
-const save = document.getElementById('save')
-const cancel = document.getElementById('cancel')
-const form = [name, nascDt,cpf, email, logradouro, bairro, number, cep, comp_end, cert_crim, est_civ]
-
-
-save.addEventListener('click', () => {
-    for (let i = 0; i < form.length; i++) {
-        if (form[i].value == ""){
-            form[i].style.borderColor = 'red'
-            form[i].setCustomValidity('Campo obrigatório')
-        } else {
-            form[i].style.borderColor = 'green'
-            form[i].required = 'false'
-        }
-    }
-})
+const cancel = document.getElementById('cancel');
 
 cancel.addEventListener('click', () => {
     window.close()
